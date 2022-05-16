19b6897b12d38cbcc11e4e13867e7c897bf9a7c2
Bruno Mestanza <brunommestanza@gmail.com>
2022-05-16 18:30:40 -0300

    add rentx project

---
+++ rentx/src/screens/Home/styles.tsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  align-items: center;
+  flex: 1;
+  justify-content: center;
+`;
+
+export const Title = styled.Text``;
